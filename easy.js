73a93294f5f37dff2ba9cb7693ae89aa1bcefddf
Ryanmoye97 <Ryan.moye1997@icloud.com>
2022-09-26 14:05:07 -0400

    first commit

---
+++ easy.js
@@ -0,0 +1,12 @@
+ // Create an array of students holding their last name, first name, and age with 3 students. To validate, please log a greeting with the first name, last name and age of the 2nd student. The output should look like "Hello, my name is John Doe and I'm 19 years old."
+
+  const greeting1 = ['Moye', 'Ryan', '25 years old'];
+  console.log(greeting1) 
+
+
+  const greeting2 = ['Wayne', 'John', '23 years old'];
+  console.log(greeting2) 
+  //Output "Hello my name is John Wayne and I am 23 years old."
+
+  const greeting3 = ['Tucker', 'Chris', '21 years old'];
+  console.log(greeting3) 
---
+++ veryeasy.js
@@ -0,0 +1,13 @@
+// Write a function named min that takes two arguments and returns their minimum.
+
+function min(num1, num2) {
+if (num1 < num2)
+return num1;
+else
+return num2;
+}
+
+console.log(min(-20, 20));
+// -20
+console.log(min(-20, 20));
+// 20
